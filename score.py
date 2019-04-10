871e6e2820241ca0baf81a20e83b35fe8059ac40
Abhishek Verma <averma@XEBIAINDIA.LOCAL>
2019-04-10 15:20:39 +0530

    refactor: v0.2

--- score.py
+++ score.py
@@ -2,14 +2,14 @@ import json, os, pickle
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-vocab =  pickle.load(open('TfidfVectorizerModel.pkl', 'rb'))
-model = joblib.load(open('MultinomialNBModel.pkl','rb'))
+vocab =  pickle.load(open("D:\Demo\Email-Automation-master\TfidfVectorizerModel.pkl", 'rb'))
+model = joblib.load(open("D:\Demo\Email-Automation-master\MultinomialNBModel.pkl", 'rb'))
 
 transformer = TfidfTransformer()
 trainedVectorizer = CountVectorizer(decode_error='replace',vocabulary=vocab)
 
 def run(raw_data):
-    vectorString  = trainedVectorizer.fit_transform([str(data)])
+    vectorString  = trainedVectorizer.fit_transform([str(raw_data)])
     transformedString = transformer.fit_transform(vectorString)
     y_hat = model.predict(transformedString)
     return(str(y_hat))
