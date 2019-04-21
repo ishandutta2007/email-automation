22d9704f8a5e8456d5d08d7b57f9893d26e204c4
Abhishek Verma <averma@XEBIAINDIA.LOCAL>
2019-04-21 17:49:09 +0530

    refactor: score.py added

--- score.py
+++ score.py
@@ -2,14 +2,16 @@ import json, os, pickle
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-vocab =  pickle.load(open("D:\Demo\Email-Automation-master\TfidfVectorizerModel.pkl", 'rb'))
-model = joblib.load(open("D:\Demo\Email-Automation-master\MultinomialNBModel.pkl", 'rb'))
+vocab =  pickle.load(open('TfidfVectorizerModel.pkl', 'rb'))
+model = joblib.load(open('MultinomialNBModel.pkl','rb'))
 
 transformer = TfidfTransformer()
 trainedVectorizer = CountVectorizer(decode_error='replace',vocabulary=vocab)
 
 def run(raw_data):
+    y_hat = dict()
     vectorString  = trainedVectorizer.fit_transform([str(raw_data)])
     transformedString = transformer.fit_transform(vectorString)
-    y_hat = model.predict(transformedString)
-    return(str(y_hat))
+    y_hat['prediction']  = model.predict(transformedString).astype(dtype=float)[0]
+    y_hat['probability'] = model.predict_proba(transformedString).astype(dtype=float).tolist()
+    return(json.dumps(y_hat))
