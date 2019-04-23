b38fd78cb765ca73ae22659d7ff974c4c304c682
llvarunll <47743109+llvarunll@users.noreply.github.com>
2019-04-23 17:44:35 +0530

    refactor: updated workflow

--- score.py
+++ score.py
@@ -10,11 +10,12 @@ def init():
     global vocab, model, trainedVectorizer, transformer
 
     # find script location
-    path = json.load(open('config.uipath'))['runtimepath']
+    #path = json.load(open('config.uipath'))['runtimepath']
+    path = "C:\Users\vbhardwaj\Documents\GitHub\Email-Automation\"
 
     # load machine learning model and meta data
-    vocab =  pickle.load(open(path +'/TfidfVectorizerModel.pkl', 'rb'))
-    model = joblib.load(open(path +'/MultinomialNBModel.pkl','rb'))
+    vocab =  pickle.load(open(path +'TfidfVectorizerModel.pkl', 'rb'))
+    model = joblib.load(open(path +'MultinomialNBModel.pkl','rb'))
 
     # init stages using meta data
     trainedVectorizer = CountVectorizer(decode_error='replace',vocabulary=vocab)
