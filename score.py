1c5a97abf39fdf8e535a372eadea7730c081b412
Abhishek Verma <averma@XEBIAINDIA.LOCAL>
2019-04-23 11:13:34 +0530

    refactor: added new project file

--- score.py
+++ score.py
@@ -1,17 +1,42 @@
+# import libraries
 import json, os, pickle
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-vocab =  pickle.load(open('TfidfVectorizerModel.pkl', 'rb'))
-model = joblib.load(open('MultinomialNBModel.pkl','rb'))
+# define init function
+def init():
+    # define global variable
+    global vocab, model, trainedVectorizer, transformer
 
-transformer = TfidfTransformer()
-trainedVectorizer = CountVectorizer(decode_error='replace',vocabulary=vocab)
+    # load machine learning model and meta data
+    vocab =  pickle.load(open('TfidfVectorizerModel.pkl', 'rb'))
+    model = joblib.load(open('MultinomialNBModel.pkl','rb'))
 
+    # init stages using meta data
+    trainedVectorizer = CountVectorizer(decode_error='replace',vocabulary=vocab)
+    transformer = TfidfTransformer()
+
+# define run function to execute the ml model
 def run(raw_data):
+    # init meta data
+    init()
+
+    # define y_hat (result) dictionary
     y_hat = dict()
-    vectorString  = trainedVectorizer.fit_transform([str(raw_data)])
-    transformedString = transformer.fit_transform(vectorString)
-    y_hat['prediction']  = model.predict(transformedString).astype(dtype=float)[0]
-    y_hat['probability'] = model.predict_proba(transformedString).astype(dtype=float).tolist()
+
+    # transform feature to feature vector
+    featureVector = trainedVectorizer.fit_transform([raw_data])
+    featureVector_fit = transformer.fit_transform(featureVector)
+
+    # save result into y_hat
+    y_hat['prediction']  = model.predict(featureVector_fit).astype(dtype=float)[0]
+    y_hat['probability'] = model.predict_proba(featureVector_fit).astype(dtype=float).tolist()
+
+    # return JSON
     return(json.dumps(y_hat))
+
+
+if __name__ == "__main__":
+    test = "This is to check machine learning model"
+    result = run(test)
+    print("Data: {}\nResult: {}".format(test,result))
