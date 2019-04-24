6162178947722be3521024b7ce8c30861aab1cb9
llvarunll <47743109+llvarunll@users.noreply.github.com>
2019-04-24 16:24:26 +0530

    refactor: updated workflow

--- score.py
+++ score.py
@@ -3,27 +3,24 @@ import json, os, pickle
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-
 # define init function
-def init():
+def init(filepath):
     # define global variable
-    global vocab, model, trainedVectorizer, transformer
+    global vocab, model, trainedVectorizer, transformer, path
 
     # find script location
-    path = json.load(open('config.uipath'))['runtimepath']
+    path = filepath
 
     # load machine learning model and meta data
-    vocab =  pickle.load(open(path + 'TfidfVectorizerModel.pkl', 'rb'))
-    model = joblib.load(open(path + 'MultinomialNBModel.pkl','rb'))
+    vocab =  pickle.load(open(path + "TfidfVectorizerModel.pkl", "rb"))
+    model = joblib.load(open(path + "MultinomialNBModel.pkl","rb"))
 
     # init stages using meta data
-    trainedVectorizer = CountVectorizer(decode_error='replace',vocabulary=vocab)
+    trainedVectorizer = CountVectorizer(decode_error="replace",vocabulary=vocab)
     transformer = TfidfTransformer()
 
 # define run function to execute the ml model
 def run(raw_data):
-    # init meta data
-    init()
 
     # define y_hat (result) dictionary
     y_hat = dict()
@@ -33,8 +30,8 @@ def run(raw_data):
     featureVector_fit = transformer.fit_transform(featureVector)
 
     # save result into y_hat
-    y_hat['prediction']  = model.predict(featureVector_fit).astype(dtype=float)[0]
-    y_hat['probability'] = model.predict_proba(featureVector_fit).astype(dtype=float).tolist()
+    y_hat["prediction"]  = model.predict(featureVector_fit).astype(dtype=float)[0]
+    y_hat["probability"] = max(max(model.predict_proba(featureVector_fit).astype(dtype=float).tolist()))
 
     # return JSON
     return(json.dumps(y_hat))
@@ -42,5 +39,7 @@ def run(raw_data):
 
 if __name__ == "__main__":
     test = "This is to check machine learning model"
+    filepath = json.load(open('config.uipath'))['filepath']
+    init(filepath)
     result = run(test)
     print("Data: {}\nResult: {}".format(test,result))
